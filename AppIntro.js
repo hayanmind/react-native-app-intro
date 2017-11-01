d4c595242ea77530395c0834290ce4963ed77c7b
Kesha Antonov <innokenty.longway@gmail.com>
2017-11-01 19:44:37 +0300

    rm unused code

--- AppIntro.js
+++ AppIntro.js
@@ -26,32 +26,6 @@ const defaultStyles = {
   container: {
     flex: 1
   },
-  header: {
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  info: {
-    flex: 0.5,
-    alignItems: 'center',
-    padding: 30,
-  },
-  slide: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-    padding: 15,
-  },
-  title: {
-    color: '#fff',
-    fontSize: 30,
-    paddingBottom: 20,
-  },
-  description: {
-    color: '#fff',
-    fontSize: 20,
-  },
   controllText: {
     color: '#fff',
     fontSize: 22,
@@ -296,37 +270,6 @@ export default class AppIntro extends PureComponent {
     );
   }
 
-  renderBasicSlidePage = (index, {
-    title,
-    description,
-    img,
-    imgStyle,
-    backgroundColor,
-    fontColor,
-    level,
-  }) => {
-    const AnimatedStyle1 = this.getTransform(index, 10, level);
-    const AnimatedStyle2 = this.getTransform(index, 0, level);
-    const AnimatedStyle3 = this.getTransform(index, 15, level);
-    const imgSource = (typeof img === 'string') ? {uri: img} : img;
-    const pageView = (
-      <View style={[this.styles.slide, { backgroundColor }]} showsPagination={false} key={index}>
-        <Animated.View style={[this.styles.header, ...AnimatedStyle1.transform]}>
-          <Image style={imgStyle} source={imgSource} />
-        </Animated.View>
-        <View style={this.styles.info}>
-          <Animated.View style={AnimatedStyle2.transform}>
-            <Text style={[this.styles.title, { color: fontColor }]}>{title}</Text>
-          </Animated.View>
-          <Animated.View style={AnimatedStyle3.transform}>
-            <Text style={[this.styles.description, { color: fontColor }]}>{description}</Text>
-          </Animated.View>
-        </View>
-      </View>
-    );
-    return pageView;
-  }
-
   renderChild = (children, pageIndex, index) => {
     const level = children.props.level || 0;
     const root = children.props.children;
@@ -358,26 +301,7 @@ export default class AppIntro extends PureComponent {
     return animatedChild;
   }
 
-  shadeStatusBarColor(color, percent) {
-    const first = parseInt(color.slice(1), 16);
-    const black = first & 0x0000FF;
-    const green = first >> 8 & 0x00FF;
-    const percentage = percent < 0 ? percent * -1 : percent;
-    const red = first >> 16;
-    const theme = percent < 0 ? 0 : 255;
-    const finalColor = (0x1000000 + (Math.round((theme - red) * percentage) + red) * 0x10000 + (Math.round((theme - green) * percentage) + green) * 0x100 + (Math.round((theme - black) * percentage) + black)).toString(16).slice(1);
-
-    return `#${finalColor}`;
-  }
-
-  isToTintStatusBar() {
-    return this.props.pageArray && this.props.pageArray.length > 0 && IS_ANDROID
-  }
-
   onMomentumScrollEnd(e, state) {
-    if (this.isToTintStatusBar()) {
-      StatusBar.setBackgroundColor(this.shadeStatusBarColor(this.props.pageArray[state.index].backgroundColor, -0.3), false);
-    }
     this.setState({ index: state.index })
     this.props.onSlideChange(state.index, state.total);
   }
@@ -398,50 +322,40 @@ export default class AppIntro extends PureComponent {
 
   render() {
     const childrens = this.props.children;
-    const { pageArray } = this.props;
     let pages = [];
     let androidPages = null;
-    if (pageArray.length > 0) {
-      pages = pageArray.map((page, i) => this.renderBasicSlidePage(i, page));
+    if (IS_IOS) {
+      pages = childrens.map((children, i) => this.renderChild(children, i, i));
     } else {
-      if (IS_IOS) {
-        pages = childrens.map((children, i) => this.renderChild(children, i, i));
-      } else {
-        pages = childrens.map((children, i) => {
-          const { transform } = this.getTransform(i, windowsWidth * 1 / 3 , 1, false);
-          return (
-            <Animated.View
-              key={i}
-              style={[
-                this.styles.animatedPage,
-                { ...transform[0] }
-              ]}
-            >
-              {this.renderChild(children, i, i)}
-            </Animated.View>
-          );
-        });
-      }
-    }
-
-    if (this.isToTintStatusBar()) {
-      StatusBar.setBackgroundColor(this.shadeStatusBarColor(this.props.pageArray[0].backgroundColor, -0.3), false);
+      pages = childrens.map((children, i) => {
+        const { transform } = this.getTransform(i, windowsWidth * 1 / 3 , 1, false);
+        return (
+          <Animated.View
+            key={i}
+            style={[
+              this.styles.animatedPage,
+              { ...transform[0] }
+            ]}
+          >
+            {this.renderChild(children, i, i)}
+          </Animated.View>
+        );
+      });
     }
 
     return (
-      <View style={this.styles.container}>
-        <Swiper
-          loop={false}
-          index={this.state.index}
-          renderPagination={this.renderPagination}
-          onMomentumScrollEnd={this.onMomentumScrollEnd}
-          onScroll={this.onScroll}
-          onPageScroll={this.onPageScroll}
-          scrollEventThrottle={16}
-        >
-          {pages}
-        </Swiper>
-      </View>
+      <Swiper
+        loop={false}
+        index={this.state.index}
+        renderPagination={this.renderPagination}
+        onMomentumScrollEnd={this.onMomentumScrollEnd}
+        onScroll={this.onScroll}
+        onPageScroll={this.onPageScroll}
+        scrollEventThrottle={16}
+        containerStyle={this.styles.container}
+      >
+        {pages}
+      </Swiper>
     );
   }
 }
@@ -455,7 +369,6 @@ AppIntro.propTypes = {
   onSkipBtnClick: PropTypes.func,
   onDoneBtnClick: PropTypes.func,
   onNextBtnClick: PropTypes.func,
-  pageArray: PropTypes.array,
   doneBtnLabel: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.element,
@@ -482,7 +395,6 @@ AppIntro.defaultProps = {
   activeDotColor: '#fff',
   rightTextColor: '#fff',
   leftTextColor: '#fff',
-  pageArray: [],
   onSlideChange: () => {},
   onSkipBtnClick: () => {},
   onDoneBtnClick: () => {},
